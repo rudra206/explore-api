7630e024d57b00f46d2563459e09c93970d356f3
rura206 <rudradipta34@gmail.com>
2025-07-18 02:13:27 +0600

    rhf

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 function loadUsers2 (){
   fetch ('https://jsonplaceholder.typicode.com/users')
   .then(response =>response.json())
-  .then(data => console.log(data)
-  )
+//   .then(data => console.log(data))
+.then( data =>displayUsers(data))
+  
+}
 
-    
+function displayUsers(data){
+    for(const users of data){
+        // console.log(users );
+        // console.log(users.email );
+        // console.log(users.address );
+        console.log(users.username );
+        
+    }
 }
