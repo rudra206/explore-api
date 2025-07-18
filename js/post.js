6e4bc0e0cff24a3fafbee03018e74d33c0f3599d
rura206 <rudradipta34@gmail.com>
2025-07-19 02:13:02 +0600

    idh

--- js/post.js
+++ js/post.js
@@ -16,14 +16,14 @@ function displayPosts(posts) {
     const postsContainer = document.getElementById('posts-container')
     for (const post of posts) {
         // console.log(post);
-
-        const postDiv = document.createElement('div');
+ const postDiv = document.createElement('div');
+ postDiv.classList.add('post')
         postDiv.innerHTML = `
-<h4> Users-</h4>
-<h5> Post: title</h5>
-<p> post Description</p>
+<h4> Users-${post.userId}</h4>
+<h5> Post: ${post.title}</h5>
+<p> post Description : ${post.body}</p>
 ` ;
         postsContainer.appendChild(postDiv);
     }
 }
-loadPost();
+loadPost(); 
